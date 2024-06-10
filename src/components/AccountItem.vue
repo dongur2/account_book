47eb604d72926e7e6a29ff7e6e03d64d787699f1
yoo <ekzmemfo@gmail.com>
2024-06-10 17:03:24 +0900

    feature: accountList 출력

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -1,28 +1,27 @@
 <template>
-  <li>
-    <span @click="console.log(penguin)">
-      {{ accountItem.category }}
-      카테고리
-    </span>
-    <span @click="console.log(penguin)">
-      {{ accountItem.title }}
-      내용
-    </span>
-    <span @click="console.log(penguin)">
-      {{ accountItem.desc }}
-      메모(필수x)
-    </span>
-
-    <span @click="console.log(penguin)">
-      {{ accountItem.type.amount }}
-      10000
-    </span>
-    <span> 원 </span>
-    <button type="button" class="btn" @click="console.log(penguin)">
-      <i class="bi bi-house" style="font-size: 3rem"></i>
+  <li class="list-group-item">
+    <span>
+      <span class="badge text-bg-secondary">
+        {{
+          accountItem.type === 'income'
+            ? accountItem.incomeCategory
+            : accountItem.expenseCategory
+        }}
+      </span> </span
+    ><br />
+    <br />
+    <br />
+    <span>{{ accountItem.title }}</span>
+    <br />
+    <span>{{ accountItem.desc }}</span
+    ><br />
+    <span>{{ accountItem.amount }}</span>
+    <span> 원 </span><br />
+    <button type="button" class="btn">
+      <i class="bi bi-house"></i>
     </button>
-    <button type="button" class="btn" @click="console.log(penguin)">
-      <i class="bi bi-house" style="font-size: 3rem"></i>
+    <button type="button" class="btn">
+      <i class="bi bi-house"></i>
     </button>
   </li>
 </template>
@@ -30,10 +29,14 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import { useAccountListStore } from '@/stores/account.js';
+
 defineProps({
-  accountItem: { Type: Object, required: true },
+  accountItem: {
+    type: Object,
+    required: true,
+  },
 });
+
 const router = useRouter();
 const accountListStore = useAccountListStore();
-//   const { deleteTodo, toggleDone } = todoListStore;
 </script>
--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,22 +1,7 @@
 <template>
-  <div
-    class="container"
-    style="
-      display: flex;
-
-      flex-direction: row;
-
-      background-color: white;
-      border-radius: 10px;
-      margin-top: 20px;
-    "
-  >
-    Calendar
-  </div>
+  <div class="container" style="background-color: white">Calendar</div>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup></script>
 
 <style></style>
