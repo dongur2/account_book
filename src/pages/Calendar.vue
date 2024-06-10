8a23174a459a3883dda7acff0628a3ee61de522f
dongur2 <holymolyguri@gmail.com>
2024-06-10 11:40:41 +0900

    Feat: FullCalendar 라이브러리 설치
    - core, daygrid, interaction, vue3 설치
    - 컴포넌트 표시 확인

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -3,20 +3,34 @@
     class="container"
     style="
       display: flex;
-
       flex-direction: row;
-
       background-color: white;
       border-radius: 10px;
       margin-top: 20px;
     "
   >
     Calendar
+    <FullCalendar :options="calendarOptions" />
+
   </div>
 </template>
 
 <script>
-export default {};
-</script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
-<style></style>
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
+</script>
