b334e66d59166820fa79a729b2bfaa725e0a9023
dongur2 <holymolyguri@gmail.com>
2024-06-11 22:48:31 +0900

    Feat: 수입/지출 목록 수정/삭제 로직 추가
    - account.js에 추가

--- src/stores/account.js
+++ src/stores/account.js
@@ -19,8 +19,56 @@ export const useAccountListStore = defineStore('accountList', () => {
     }
   };
 
+
+  /* 
+   * 목록 수정 
+   */
+  const modifyAccount = async ({id, type, category, title, desc, amount, date}, successCallBack) => {
+    try {
+      const payload = {};
+      if (type === 'income') payload = {id, type, incomeCategory: category, title, desc, amount, date};
+      else payload = {id, type, expenseCategory: category, title, desc, amount, date};
+
+      const res = await axios.put(`${BASEURI}/${id}`, payload); // PUT
+      
+      if(res.data.state === 'success') {
+          let idx = state.accountList.findIndex((item) => item.id === id);
+          if(idx > -1) {
+            state.accountList[idx] = payload; // local update & rendering
+            successCallBack();
+
+          } else throw Exception('None');
+
+      } else {
+          console.log('Failed to modify');
+      }
+    } catch (err) {
+      console.log('Err: '+err);
+    }
+  }
+  
+  /* 
+   * 목록 삭제
+   */
+  const deleteAccount = async (id, successCallBack) => {
+    try {
+      const res = await axios.delete(`${BASEURI}/${id}`); // delete
+
+      if(res.data.state === 'success') {
+        let idx = state.accountList.findIndex((item) => item.id === id);
+        state.accountList.splice(idx, 1); // local delete & rendering
+        successCallBack();
+
+      } else {
+        console.log('Failed to delete');
+      }
+    } catch (err) {
+      console.log('Err: '+err);
+    }
+  }
+
   const accountList = computed(() => state.accountList);
   const isLoading = computed(() => state.isLoading);
 
-  return { accountList, isLoading, fetchAccountList };
+  return { accountList, isLoading, fetchAccountList, modifyAccount, deleteAccount };
 });
