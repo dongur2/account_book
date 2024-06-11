5fe9e4f91f8b10f6a47e466eddbf6e4ea34873dc
kim ye jong <yejanot@naver.com>
2024-06-11 16:16:20 +0900

    feat.페이지 버튼 css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="container" style="justify-content: space-between">
+    <!-- 좌측 PieChart 컴포넌트 -->
     <div style="flex: 1; justify-content: center">
       <PieChart />
     </div>
-
+    <!-- 중앙 섹션: 아이콘 및 수입/지출 정보 -->
     <div style="flex-direction: column; flex: 1">
-      <div style="flex: 1">
-        <div class="amount" style="flex: 1">수입:income{{}}</div>
-        <div class="amount" style="flex: 1">지출:expense{{}}</div>
+      <!-- 아이콘 부분 -->
+      <div style="display: flex; justify-content: flex-end; margin: 1rem 0">
+        <!-- 좌측 화살표 버튼 -->
+        <button>
+          <i class="bi bi-chevron-double-left" style="font-size: inherit"></i>
+        </button>
+        <!-- 중앙 하우스 아이콘 버튼 -->
+        <button>
+          <i class="bi bi-house" style="font-size: inherit"></i>
+        </button>
+        <!-- 우측 화살표 버튼 -->
+        <button>
+          <i class="bi bi-chevron-double-right" style="font-size: inherit"></i>
+        </button>
       </div>
-      <hr class="my-4" />
-      <div style="flex: 1">
-        <div style="flex: 1">category{{}}</div>
-        <div style="flex: 1">amount{{}}</div>
+
+      <!-- 수입/지출 정보 -->
+      <div style="flex: 2">
+        <div class="amount" style="flex: 1; justify-content: center">
+          수입:income{{}}
+        </div>
+        <div class="amount" style="flex: 1; justify-content: center">
+          지출:expense{{}}
+        </div>
+      </div>
+      <hr class="my-3" />
+      <!-- 카테고리 및 금액 정보 -->
+      <div style="flex: 2">
+        <div style="flex: 1; align-items: center; justify-content: center">
+          category{{}}
+        </div>
+        <div style="flex: 1; align-items: center; justify-content: center">
+          amount{{}}
+        </div>
       </div>
     </div>
   </div>
-
+  <!-- 입력 섹션 -->
   <div>
-    <!-- inputIncomeExpense -->
+    <!-- InputIncomeExpense 컴포넌트 -->
     <InputIncomeExpense />
   </div>
+
+  <!-- 계좌 목록 섹션 -->
   <div class="container">
+    <!-- AccountList 컴포넌트 -->
     <AccountList />
   </div>
 </template>
 
 <script setup>
 import PieChart from '@/components/PieChart.vue';
-
 import InputIncomeExpense from '@/components/InputIncomeExpense.vue';
 import AccountList from '../components/AccountList.vue';
 </script>
+<style>
+button {
+  background: none;
+  border: none;
+  padding: 0;
+  font-size: 1rem;
+  margin-right: 1rem;
+  display: flex;
+  align-items: center;
+}
+</style>
