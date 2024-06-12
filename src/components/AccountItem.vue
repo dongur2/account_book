b37542150f9d99a773032b238c2274911e0569ba
owzl <alliwannado1211@gmail.com>
2024-06-12 14:07:28 +0900

    Fix: 프리티어 땜에 바뀌어서 어쩔 수 없이 커밋

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -9,22 +9,26 @@
       padding: 0;
     "
   >
-
     <div v-if="isModifying" style="flex: 1; align-items: center">
-      <select name="type" v-model="accountItem.type" 
-      @change="accountItem.incomeCategory = 'salary'; accountItem.expenseCategory = 'fixed'">
+      <select
+        name="type"
+        v-model="accountItem.type"
+        @change="
+          accountItem.incomeCategory = 'salary';
+          accountItem.expenseCategory = 'fixed';
+        "
+      >
         <option value="income">수입</option>
         <option value="expense">지출</option>
       </select>
     </div>
 
-    <div v-if="!isModifying" style="flex: 1; align-items: center;">
+    <div v-if="!isModifying" style="flex: 1; align-items: center">
       <p>{{ accountItem.date }}</p>
     </div>
-    <div v-else style="flex: 1; align-items: center;">
-      <input type="date" v-model="accountItem.date">
+    <div v-else style="flex: 1; align-items: center">
+      <input type="date" v-model="accountItem.date" />
     </div>
-  
 
     <div v-if="!isModifying" style="flex: 1">
       <span>
@@ -38,7 +42,11 @@
       </span>
     </div>
     <div v-else style="flex: 1; align-items: center">
-      <select v-if="accountItem.type === 'income'" name="category" v-model="accountItem.incomeCategory">
+      <select
+        v-if="accountItem.type === 'income'"
+        name="category"
+        v-model="accountItem.incomeCategory"
+      >
         <option value="salary">근로소득</option>
         <option value="pin">용돈</option>
         <option value="etc">기타</option>
@@ -56,29 +64,49 @@
       <p>{{ accountItem.title }}</p>
     </div>
     <div v-else style="flex: 1; align-items: center">
-      <input type="text" v-model="accountItem.title" placeholder="내역을 입력하세요">
+      <input
+        type="text"
+        v-model="accountItem.title"
+        placeholder="내역을 입력하세요"
+      />
     </div>
 
     <div v-if="!isModifying" style="flex: 1; align-items: center">
       <p>{{ accountItem.desc }}</p>
     </div>
     <div v-else style="flex: 1; align-items: center">
-      <input type="text" v-model="accountItem.desc" placeholder="메모를 입력하세요(선택)">
+      <input
+        type="text"
+        v-model="accountItem.desc"
+        placeholder="메모를 입력하세요(선택)"
+      />
     </div>
 
     <div v-if="!isModifying" style="flex: 1; align-items: center">
       <p>{{ moneyFormat(accountItem.amount) }}</p>
     </div>
     <div v-else style="flex: 1; align-items: center">
-      <input type="text" v-model="accountItem.amount" placeholder="금액을 입력하세요">
+      <input
+        type="text"
+        v-model="accountItem.amount"
+        placeholder="금액을 입력하세요"
+      />
     </div>
 
     <div style="flex: 1; align-items: center; justify-content: end">
-      <button type="button" class="btn" @click="isModifying ? modifyAccountHandler(accountItem):changeInput()">
-        <i class="bi bi-house"></i>
+      <button
+        type="button"
+        class="btn"
+        @click="isModifying ? modifyAccountHandler(accountItem) : changeInput()"
+      >
+        수정
       </button>
-      <button type="button" class="btn" @click="deleteAccountHandler(accountItem.id)">
-        <i class="bi bi-house"></i>
+      <button
+        type="button"
+        class="btn"
+        @click="deleteAccountHandler(accountItem.id)"
+      >
+        삭제
       </button>
     </div>
   </li>
@@ -89,7 +117,7 @@ import { useRouter } from 'vue-router';
 import { useAccountListStore } from '@/stores/account.js';
 import { useCalendarAccountStore } from '@/stores/calendarAccount'; //calendar
 import { moneyFormat } from '../utils/moneyFormat';
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 // 컴포넌트 속성 정의
 defineProps({
@@ -109,10 +137,10 @@ const calendarAccountStore = useCalendarAccountStore(); //calendar
 /* 목록 제거 */
 const { deleteAccount } = accountListStore;
 const deleteAccountHandler = (id) => {
-  if(confirm('해당 항목을 삭제하시겠습니까?')) {
+  if (confirm('해당 항목을 삭제하시겠습니까?')) {
     deleteAccount(id, fetchSummaryList); //callback: fetchCalendarSummary
   }
-}
+};
 
 /* 목록 수정 */
 const isModifying = ref(false);
@@ -121,19 +149,21 @@ const { fetchSummaryList } = calendarAccountStore;
 
 const changeInput = () => {
   isModifying.value = true;
-}
+};
 
 const modifyAccountHandler = (item) => {
-    if(item.title === '' || item.title.trim() === '') { //title not null
-      alert('내역을 입력해주세요.');
-    } else if(item.amount === '' || item.amount.trim() === '') { //amount not null
-      alert('금액을 입력해주세요.');
-    } else if(parseFloat(item.amount) <= 0 || parseFloat(item.amount) % 1 > 0) { //amount > 0 && amount is Integer
-      alert('금액은 1원 이상부터 입력 가능합니다. (소수점 불가)');
-    } else {
-      modifyAccount(item, fetchSummaryList); //callback: fetchCalendarSummary
-      isModifying.value = false;
-    }
-}
-
+  if (item.title === '' || item.title.trim() === '') {
+    //title not null
+    alert('내역을 입력해주세요.');
+  } else if (item.amount === '' || item.amount.trim() === '') {
+    //amount not null
+    alert('금액을 입력해주세요.');
+  } else if (parseFloat(item.amount) <= 0 || parseFloat(item.amount) % 1 > 0) {
+    //amount > 0 && amount is Integer
+    alert('금액은 1원 이상부터 입력 가능합니다. (소수점 불가)');
+  } else {
+    modifyAccount(item, fetchSummaryList); //callback: fetchCalendarSummary
+    isModifying.value = false;
+  }
+};
 </script>
